f78ccfd88e7665836e6bd4d55397247fa5836fa6
kaiser24 <damsog38@gmail.com>
2022-02-22 22:27:28 -0500

    Adding input parameters to LPR module

--- LPR.py
+++ LPR.py
@@ -1,4 +1,7 @@
+import argparse
 from ctypes import *
+import json
+from tkinter.messagebox import NO
 import cv2
 import numpy as  np
 import time
@@ -8,9 +11,11 @@ from LPDetector import LPDetector
 from logger import Logger
 
 class LPR:
-    def __init__(self, LOGGER_LEVEL="DEBUG", SHOW_TIME=True) -> None:
+    def __init__(self, LOGGER_LEVEL="DEBUG", SHOW_TIME=True, detection_zone=None) -> None:
         self.LOGGER_LEVEL = LOGGER_LEVEL
         self.MODULE_NAME = "LPR MODULE"
+
+        self.SHOW_TIME = SHOW_TIME
 
         self.lpocr_processor = LPOcr()
         self.lp_detector = LPDetector()
@@ -33,9 +38,9 @@ class LPR:
         # Applying LP Detector
         prev_time_det = time.time()
         self.logger.debug("Applying LP detection on image")
-        license_pnts_list,license_img_list,_ = lpr.lp_detector.applyLPDetection(image, 0.5)
-        if SHOW_TIME:
-            lpr.logger.debug(f'LPDetection time {time.time()-prev_time_det}')
+        license_pnts_list,license_img_list,_ = self.lp_detector.applyLPDetection(image, 0.5)
+        if self.SHOW_TIME:
+            self.logger.debug(f'LPDetection time {time.time()-prev_time_det}')
         
 
         for i,license_plate_img in enumerate(license_img_list):
@@ -54,17 +59,15 @@ class LPR:
             cv2.imwrite('photo.jpg',license_plate_img)
             prev_time_ocr = time.time()
             ocrResult = self.lpocr_processor.applyOCR(b"photo.jpg",0.4)
-            if SHOW_TIME:
-                lpr.logger.debug(f'LPOCR time {time.time() - prev_time_ocr}')
+            if self.SHOW_TIME:
+                self.logger.debug(f'LPOCR time {time.time() - prev_time_ocr}')
             
             prev_time_lpro = time.time()
 
 
-            if SHOW_TIME:
-                lpr.logger.debug(f'LP Reorganized time {time.time() - prev_time_lpro}')
+            if self.SHOW_TIME:
+                self.logger.debug(f'LP Reorganized time {time.time() - prev_time_lpro}')
             
-            self.logger.debug(f'LP reorganized {ocrResult}')
-
             lplate = { "label" : " ".join(map(str,ocrResult)), "bbox" : (lp_points) }
             self.logger.debug(f'LP image result {lplate}')
 
@@ -73,34 +76,58 @@ class LPR:
         self.json_output = {"plates" : output_array }
 
         return self.json_output
-            
 
-if __name__ == "__main__":
+def main() -> None:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, help="Media source. Video, Stream or Image")
+    ap.add_argument("-iz", "--zone", required=False, help="""Points that determine the zone where to reduce the detection. as a json. Exmaple: 
+                                                             Example: '[{"x": 10, "y": 10}, {"x": 100, "y": 120},{...}]' """)
+    ap.add_argument("-dz", "--draw_zone", action="store_true", help="If selected allows to draw the input zone manually. this option ignores the flag -iz")
+    ap.add_argument("-d", "--debug", action="store_true", help="Sets Logger level to debug")
+    args= vars(ap.parse_args())
+    # Commad Example
+    # python3 LPR.py -i '/mnt/72086E48086E0C03/WORK_SPACE/Lpr/test_videos/test2.webm'
+
+    MODULE_NAME = "LPR STANDALONE MODULE"
+
+    if args["debug"]:
+        logger = Logger("DEBUG", TAG_MODULE=MODULE_NAME)
+    else:
+        logger = Logger("INFO", TAG_MODULE=MODULE_NAME)
+
+    if args["zone"]:
+        logger.info(f'Zone input: {args["zone"]}')
+        zone = json.loads(args["zone"])
+    if args["draw_zone"]:
+        logger.info(f'Draw Zone selected. ignoring Zone input')
+
+        media=cv2.VideoCapture(args["input"])
+
+        ret, frame = media.read()
+        inputZones = selectPolygonZone(frame,'green')
+        inputZones = inputZones[0]
+        #polizone = Polygon(  [inputZones[0], inputZones[1], inputZones[2], inputZones[3]] )    
+        media.close()
+        logger.info(f'Drawn Zone: {inputZones[0]}')
+
 
     SHOW_TIME = True
     RESIZE = 0.55
 
     lpr = LPR()
+    media=cv2.VideoCapture(args["input"])
 
-    cap=cv2.VideoCapture('/mnt/72086E48086E0C03/WORK_SPACE/Lpr/test_videos/test2.webm')
-
-    ret, frame = cap.read()
-    inputZones = []
-    inputZones = selectPolygonZone(frame,'green')
-    inputZones = inputZones[0]
-    polizone = Polygon(  [inputZones[0], inputZones[1], inputZones[2], inputZones[3]] )
-    zone_pts = np.array([ [inputZones[0][0],inputZones[0][1]] ,[inputZones[1][0],inputZones[1][1]] , [inputZones[2][0],inputZones[2][1]], [inputZones[3][0],inputZones[3][1]] ])
-    
-    while cap.isOpened():
-        ret, image = cap.read()
+    while media.isOpened():
+        ret, image = media.read()
         if not ret:
-            lpr.logger.error('Error reading frame or video. exiting')
+            logger.error('Error reading frame or video. exiting')
             break
         im2show = image.copy()
 
-        cv2.polylines(im2show, [zone_pts], True, (0,255,0), 2)
+        #cv2.polylines(im2show, [zone_pts], True, (0,255,0), 2)
 
-        lpr.applyLPR(image)
+        license_plates_json = lpr.applyLPR(image)
+        logger.debug(f'LPR Image result: {license_plates_json}')
             
         im2show = cv2.resize(im2show, ( int(im2show.shape[1]*RESIZE),int(im2show.shape[0]*RESIZE) ), interpolation = cv2.INTER_AREA) 
         cv2.imshow("origi",im2show)
@@ -111,3 +138,7 @@ if __name__ == "__main__":
         #cv2.waitKey(0)
         
         #for det in r[0]:
+
+
+if __name__ == "__main__":
+    main()
