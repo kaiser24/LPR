405d79b4ed9f52b6d56d8c6be44927e4d142d54d
kaiser24 <damsog38@gmail.com>
2022-02-23 22:57:49 -0500

    Reorganized the main LPR method and extracted a method which filters the plates if an area was drawn

--- LPR.py
+++ LPR.py
@@ -1,4 +1,5 @@
 import argparse
+import base64
 from ctypes import *
 import json
 from tkinter.messagebox import NO
@@ -47,10 +48,47 @@ class LPR:
         inputZones = [ (point["x"],point["y"]) for point in zone ]
         zonePoints = np.array([ [inputZones[0][0],inputZones[0][1]] ,[inputZones[1][0],inputZones[1][1]] , [inputZones[2][0],inputZones[2][1]], [inputZones[3][0],inputZones[3][1]] ])
         return zonePoints
+    
+    def img2b64(self, img):
+        retval, buffer = cv2.imencode('.jpg', img)
+        jpg_as_text = base64.b64encode(buffer)
+        return jpg_as_text
 
-    def applyLPR(self, image):
+    # Method to Filter the license plates that are outside the area of interest. if no area drawn then just pass all of them
+    def filterDetsArea(self, lp_slice_images_list, lp_slice_points_list, width, height):
+        lp_images_list = []
+        lp_points_list = []
 
-        # Initiallizations
+        if( self.detectionPolygon is not None ):
+
+            for i,(license_plate_img,lp_slice_points) in enumerate(zip(lp_slice_images_list, lp_slice_points_list)):
+                self.logger.debug(f'Licence plate: {i}')
+                license_pnts = lp_slice_points.pts
+                lp_points = int(license_pnts[0][0]*width),int(license_pnts[1][1]*height),int(license_pnts[0][2]*width),int(license_pnts[1][3]*height)
+                lp_center = (int( (license_pnts[0][0]*width + license_pnts[0][2]*width)/2 ), int( ( license_pnts[1][1]*height+ license_pnts[1][3]*height)/2) )
+
+                if( self.detectionPolygon.contains( Point( lp_center)  ) ):
+                    lp_images_list.append(license_plate_img)
+
+                    self.logger.debug(f'Licence plate: {i} obtaining points : {lp_points}')
+                    lp_points_list.append(lp_points)
+
+            return lp_images_list, lp_points_list
+        else:  
+            for i,(license_plate_img,lp_slice_points) in enumerate(zip(lp_slice_images_list, lp_slice_points_list)):
+                license_pnts = lp_slice_points.pts
+                lp_points = int(license_pnts[0][0]*width),int(license_pnts[1][1]*height),int(license_pnts[0][2]*width),int(license_pnts[1][3]*height)
+                
+                self.logger.debug(f'Licence plate: {i} obtaining points : {lp_points}')
+                lp_points_list.append(lp_points)
+
+            return lp_slice_images_list, lp_points_list
+
+    def applyLPR(self, image, return_image=False, return_img_json=False):
+
+        # Initializations
+        self.img = image
+        img2show = self.img.copy()
         self.json_output = {}
         output_array = []
         
@@ -59,24 +97,23 @@ class LPR:
         # Applying LP Detector
         prev_time_det = time.time()
         self.logger.debug("Applying LP detection on image")
-        license_pnts_list,license_img_list,_ = self.lp_detector.applyLPDetection(image, 0.5)
+        lp_slice_poinsts_list,lp_slice_images_list,_ = self.lp_detector.applyLPDetection(image, 0.5)
         if self.SHOW_TIME:
             self.logger.debug(f'LPDetection time {time.time()-prev_time_det}')
         
+        if self.detectionZone is not None:
+            cv2.polylines(img2show, [self.detectionZonePoints], True, (170,205,102), 2)        
 
-        for i,license_plate_img in enumerate(license_img_list):
-            self.logger.debug(f'Licence plate: {i}')
-            license_plate_img = license_img_list[i]
-            license_pnts = license_pnts_list[i].pts
-            lp_points = int(license_pnts[0][0]*width),int(license_pnts[1][1]*height),int(license_pnts[0][2]*width),int(license_pnts[1][3]*height)
+        lp_images_list, lp_points_list = self.filterDetsArea(lp_slice_images_list, lp_slice_poinsts_list, width, height)
 
+        for i,(license_plate_img,lp_points) in enumerate(zip(lp_images_list, lp_points_list)):
             self.logger.debug(f'Licence plate: {i} points {lp_points}')
-
-            #cv2.rectangle(im2show, (lp_points[0],lp_points[1]),(lp_points[2],lp_points[3]), (0,255,0), 2)
+            cv2.rectangle(img2show, (lp_points[0],lp_points[1]),(lp_points[2],lp_points[3]), (128,128,0), 2)
             
             license_plate_img=license_plate_img*255
             license_plate_img=license_plate_img.astype(np.uint8)
-            
+
+            # Why do we have to write the image to disk for yolo to process it? 
             cv2.imwrite('photo.jpg',license_plate_img)
             prev_time_ocr = time.time()
             ocrResult = self.lpocr_processor.applyOCR(b"photo.jpg",0.4)
@@ -96,7 +133,10 @@ class LPR:
         
         self.json_output = {"plates" : output_array }
 
-        return self.json_output
+        if return_image:
+            return self.json_output, img2show
+        else:
+            return self.json_output
 
 def list2json(points_list):
     output_json = []
@@ -114,11 +154,12 @@ def main() -> None:
     ap.add_argument("-iz", "--zone", required=False, help="""Points that determine the zone where to reduce the detection. as a json. Exmaple: 
                                                              Example: '[{"x": 10, "y": 10}, {"x": 100, "y": 120},{...}]' """)
     ap.add_argument("-dz", "--draw_zone", action="store_true", help="If selected allows to draw the input zone manually. this option ignores the flag -iz")
+    ap.add_argument("-s", "--show_img", action="store_true", help="Shows image output")
     ap.add_argument("-d", "--debug", action="store_true", help="Sets Logger level to debug")
     args= vars(ap.parse_args())
     # Commad Example
     # python3 LPR.py -i '/mnt/72086E48086E0C03/WORK_SPACE/Lpr/test_videos/test2.webm' -iz '[{"x": 150, "y": 217}, {"x": 97, "y": 308}, {"x": 561, "y": 299}, {"x": 551, "y": 227}, {"x": 150, "y": 217}]'
-    # python3 LPR.py -i '/mnt/72086E48086E0C03/WORK_SPACE/Lpr/test_videos/test2.webm' -dz
+    # python3 LPR.py -i '/mnt/72086E48086E0C03/WORK_SPACE/Lpr/test_videos/test2.webm' -dz -s
 
     MODULE_NAME = "LPR STANDALONE MODULE"
 
@@ -167,14 +208,21 @@ def main() -> None:
 
         #cv2.polylines(im2show, [zone_pts], True, (0,255,0), 2)
 
-        license_plates_json = lpr.applyLPR(image)
-        logger.debug(f'LPR Image result: {license_plates_json}')
-            
-        im2show = cv2.resize(im2show, ( int(im2show.shape[1]*RESIZE),int(im2show.shape[0]*RESIZE) ), interpolation = cv2.INTER_AREA) 
-        cv2.imshow("origi",im2show)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+        if args["show_img"]:
+            license_plates_json, img = lpr.applyLPR(image, return_image=True)
+            logger.debug(f'LPR Image result: {license_plates_json}')
+                
+            cv2.namedWindow('Frame')
+            cv2.imshow('Frame', img)
+
+            fin = cv2.waitKey(1) & 0xFF
+            if(fin == ord('q')):
+                logger.info("Terminate key pressed. Closing program")
+                break
+        else:
+            license_plates_json = lpr.applyLPR(image, return_image=False)
+            logger.debug(f'LPR Image result: {license_plates_json}')
+
 
 if __name__ == "__main__":
     main()
