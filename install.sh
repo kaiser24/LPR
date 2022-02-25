2700ce3dd7749f9eb317e629c42b972dbf0d0354
felipe.serna <damsog38@gmail.com>
2022-02-25 09:00:54 -0500

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
 #python3 -m pip install virtualenv
-virtualenv lpr_venv
+virtualenv lpr_venv --python=python3.7
 source lpr_venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install opencv-contrib-python
@@ -11,6 +13,7 @@ python3 -m pip install roipoly
 python3 -m pip install shapely
 python3 -m pip install tensorflow==1.14.0
 python3 -m pip install tensorflow-gpu==1.14.0
+python3 -m pip install keras==2.3
 python3 -m pip install h5py==2.10.0
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$PATH"
 export XLA_FLAGS='--xla_gpu_cuda_data_dir=/usr/local/cuda/'
